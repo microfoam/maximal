07e8a5efbbd2a1be7e673d3b740e29957e21ac2d
Albert <albert-erives@uiowa.edu>
2020-06-14 21:43:22 -0500

    Changes/updates to usage copy

--- src/microhomology.h
+++ src/microhomology.h
@@ -1482,9 +1482,9 @@ void usage(char *usage_version)
 	}
 	printf("\n\n");
 	printf("  *   This option takes an optional number argument with a monotonically-increasing effect.\n");
-	printf("  *#  This option takes an optional number argument specifying which mode to use among a set of size #.\n");
-	printf("\n Usage examples: ./maximal -v  sequence_file.txt              [Run in verbose user mode]");
-	printf("\n                 ./maximal -Kn sequence_file.txt -X 2 -Y 800  [Show consensus row; no relax-2D; Fisher-Yates scrambled to length 800 bp]\n");
+	printf("  *x  This option takes an optional number argument specifying which mode to use among a set of size x.\n");
+	printf("\n Usage examples: ./maximal -v 1 sequence_file.txt              [Run in verbose user mode]");
+	printf("\n                 ./maximal -Kn  sequence_file.txt -X 2 -Y 800  [Show consensus row; no relax-2D; Fisher-Yates scrambled to length 800 bp]\n");
 	printf("\n Citations:");
 	printf("\n  (1) Erives, A. J. (2018) Genetic sequences are two-dimensional. bioRxiv 2018.");
 	printf("\n      https://doi.org/10.1101/299867. CC-BY 4.0 International license.");
