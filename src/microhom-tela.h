7e4069da65eee95145e23b9402d2a843aee9a34d
Albert <albert-erives@uiowa.edu>
2020-06-24 13:25:51 -0500

    Changes to next_k() first motivated by need for opt_x dependence as shown by a PISO 4 nugget, second for clarity. Benchmarks mostly the same except a slight wiggle in cleanup_set-all WCR avg.

--- src/microhom-tela.h
+++ src/microhom-tela.h
@@ -429,10 +429,12 @@ int next_k(int n, int k1, short unsigned int seqtype)
 				if (seqtype==1 && tela[n+i].c == ambig.sym)
 					break;
 				if (tela[m+i].c != tela[n+i].c) {
-					if (k>PISO+1 && tela[m+i].e == tela[n+i].e) {
-						if (++transits <= maxtransits) 
-							;
-						else
+					if (opt_x.bit && k>PISO && tela[m+i].e == tela[n+i].e) {
+						if (++transits > maxtransits) 
+							break;
+					}
+					else if (k>PISO && tela[m+i].e == tela[n+i].e) {
+						if (++transits >= maxtransits) 
 							break;
 					}
 					else
